541b42af8df4556b73b0072111fcfa7754ce9f0c
Steven Emmanuel <ksteven925@gmail.com>
2018-12-13 13:09:34 +0530

    Initial File Upload
    working prototype of procedural terrain generation

---
+++ Assets/Scripts/CaveTile.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CaveTile : TerrainTile {
+
+
+#if UNITY_EDITOR
+    [MenuItem("Assets/Create/CaveTile",false,1)]
+        private static void CreateIslandTile()
+    {
+
+        CaveTile myAT = new CaveTile();
+        Sprite[] myTextures = myAT.GetSprite(); 
+
+        if (myTextures != null)
+        { 
+            Debug.Log(myTextures.GetType() + "Length: " + myTextures.Length );
+            Debug.Log(myTextures[0].name);
+            }
+        else
+        {
+            Debug.Log("Texture not loaded");
+        }
+
+        string fName2 = "CaveTile";
+
+        myAT.name = fName2 + ".asset";
+        //Has to be placed before Create of Asset data will be lost after saving
+        myAT.InitiateSlots(myTextures);
+        AssetDatabase.CreateAsset(myAT, "Assets/Tiles/" +myAT.name + "");
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+    //A preview of the tile
+    [SerializeField]
+    private Sprite preview;
+
+
+    public Sprite[] GetSprite()
+    {
+        Sprite[] myTextures = Resources.LoadAll<Sprite>("Textures/cave/rock") ;
+        return myTextures;
+    }
+#endif
+
+
+    //Heritage from TerrainTile could do this with  GetTileData too?
+    public override Sprite[] GetSprite(string textName)
+    {
+        Sprite[] myTextures= base.GetSprite(textName);
+        return myTextures;
+    }
+
+
+}
+
+
+
---
+++ Assets/Scripts/TileAutomata.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+public class TileAutomata : MonoBehaviour {
+
+
+    [Range(0,100)]
+    public int iniChance;
+    [Range(1,8)]
+    public int birthLimit;
+    [Range(1,8)]
+    public int deathLimit;
+    [Range(1,10)]
+    public int numR;
+    private int count = 0;
+
+    private int[,] terrainMap;
+    public Vector3Int tmpSize;
+    public Tilemap topMap;
+    public Tilemap botMap;
+    public TerrainTile topTile;
+    public AnimatedTile botTile;
+
+    int width;
+    int height;
+
+    public void DoSim(int nu)
+    {
+        ClearMap(false);
+        width = tmpSize.x;
+        height = tmpSize.y;
+
+        if (terrainMap==null)
+            {
+            terrainMap = new int[width, height];
+            InitPos();
+            }
+
+
+        for (int i = 0; i < nu; i++)
+        {
+            terrainMap = GenTilePos(terrainMap);
+        }
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (terrainMap[x, y] == 1)
+                    topMap.SetTile(new Vector3Int(-x + width / 2, -y + height / 2, 0), topTile);
+                botMap.SetTile(new Vector3Int(-x + width / 2, -y + height / 2, 0), botTile);
+                    
+
+            }
+        }
+    }
+
+    public void InitPos()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                terrainMap[x, y] = Random.Range(1, 101) < iniChance ? 1 : 0;
+            }
+
+        }
+
+    }
+
+
+    public int[,] GenTilePos(int[,] oldMap)
+    {
+        int[,] newMap = new int[width,height];
+        int neighb;
+        BoundsInt myB = new BoundsInt(-1, -1, 0, 3, 3, 1);
+
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                neighb = 0;
+                foreach (var b in myB.allPositionsWithin)
+                {
+                    if (b.x == 0 && b.y == 0) continue;
+                    if (x+b.x >= 0 && x+b.x < width && y+b.y >= 0 && y+b.y < height)
+                    {
+                        neighb += oldMap[x + b.x, y + b.y];
+                    }
+                    else
+                    {
+                        neighb++;
+                    }
+                }
+
+                if (oldMap[x,y] == 1)
+                {
+                    if (neighb < deathLimit) newMap[x, y] = 0;
+
+                        else
+                        {
+                            newMap[x, y] = 1;
+
+                        }
+                }
+
+                if (oldMap[x,y] == 0)
+                {
+                    if (neighb > birthLimit) newMap[x, y] = 1;
+
+                else
+                {
+                    newMap[x, y] = 0;
+                }
+                }
+
+            }
+
+        }
+
+
+
+        return newMap;
+    }
+
+
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+            {
+            DoSim(numR);
+            }
+
+
+        if (Input.GetMouseButtonDown(1))
+            {
+            ClearMap(true);
+            }
+
+
+
+        if (Input.GetMouseButton(2))
+        {
+            SaveAssetMap();
+            count++;
+        }
+
+
+
+
+
+
+
+
+        }
+
+
+    public void SaveAssetMap()
+    {
+        string saveName = "tmapXY_" + count;
+        var mf = GameObject.Find("Grid");
+
+        if (mf)
+        {
+            var savePath = "Assets/" + saveName + ".prefab";
+            if (PrefabUtility.CreatePrefab(savePath,mf))
+            {
+                EditorUtility.DisplayDialog("Tilemap saved", "Your Tilemap was saved under" + savePath, "Continue");
+            }
+            else
+            {
+                EditorUtility.DisplayDialog("Tilemap NOT saved", "An ERROR occured while trying to saveTilemap under" + savePath, "Continue");
+            }
+
+
+        }
+
+
+    }
+
+    public void ClearMap(bool complete)
+    {
+
+        topMap.ClearAllTiles();
+        botMap.ClearAllTiles();
+        if (complete)
+        {
+            terrainMap = null;
+        }
+
+
+    }
+
+
+
+}
